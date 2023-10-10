a1e54fbb0b0a982245ae52bb59058761a5f6acb7
willian-leal <willian.oliveira@bandtec.com.br>
2023-10-10 15:49:14 -0300

    subindo exercícios feitos em aula

---
+++ sistema-smart-tv/src/Teste.java
@@ -0,0 +1,11 @@
+public class Teste {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        for (int x = 1; x < 2; x++) {
+            numero = numero + x;
+        }
+        System.out.println("O valor de número é: " + numero);
+    }
+
+}
