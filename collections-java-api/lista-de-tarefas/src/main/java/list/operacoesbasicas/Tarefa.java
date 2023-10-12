fbb59f232052b559f1362632640b2a11ca4f9aeb
willian-leal <willian.oliveira@bandtec.com.br>
2023-10-12 15:02:56 -0300

    subindo exercício da aula de List

---
+++ collections-java-api/lista-de-tarefas/src/main/java/list/operacoesbasicas/Tarefa.java
@@ -0,0 +1,19 @@
+package main.java.list.operacoesbasicas;
+
+public class Tarefa {
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
